c7b7b2ab143529129ae9a3f91497aea424a3d00f
Okoyedennis <okoyedennis7@gmail.com>
2022-07-21 23:51:15 +0100

    springboot blog api

---
+++ src/main/java/com/springboot/blog/Dto/CommentDto.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.Dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+public class CommentDto {
+    private long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
+    private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
+    private String email;
+
+    @NotEmpty
+    @Size(min = 10, message = "Comment body must be minimum of 10 character")
+    private String body;
+}
---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,121 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.Dto.PostDto;
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.Dto.PostResponse;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    private ModelMapper mapper;
+
+    public PostServiceImpl(PostRepository postRepository, ModelMapper mapper) {
+        this.postRepository = postRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+        return mapToDto(newPost);
+    }
+
+    @Override
+    public PostResponse getAllPost(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
+        //Create Pageable instance
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Post> posts = postRepository.findAll(pageable);
+
+        //Get content for page object
+        List<Post> listOfPosts = posts.getContent();
+
+        List<PostDto> content = listOfPosts.stream().sorted(new compare()).map(this::mapToDto).collect(Collectors.toList());
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(content);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+
+        return postResponse;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        return mapToDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatedPost = postRepository.save(post);
+
+        return mapToDto(updatedPost);
+    }
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+        postRepository.delete(post);
+    }
+
+    private PostDto mapToDto(Post post){
+
+//        PostDto postDto = new PostDto();
+//        postDto.setId(post.getId());
+//        postDto.setTitle(post.getTitle());
+//        postDto.setContent(post.getContent());
+//        postDto.setDescription(post.getDescription());
+
+        return mapper.map(post, PostDto.class);
+    }
+
+    private Post mapToEntity(PostDto postDto){
+        //        Post post = new Post();
+//        post.setTitle(postDto.getTitle());
+//        post.setDescription(postDto.getDescription());
+//        post.setContent(postDto.getContent());
+
+        return mapper.map(postDto, Post.class);
+    }
+
+
+ private static class compare implements Comparator<Post>{
+
+     @Override
+     public int compare(Post o1, Post o2) {
+         if (o1.getId() < o2.getId()){
+             return -1;
+         }
+         return 0;
+     }
+ }
+
+}
